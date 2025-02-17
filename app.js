f2b4bd7afea16af4fb3ba8a74f39ea08620abdcf
BJN <bijenmaharjan55@gmail.com>
2025-02-17 23:14:39 +0545

    gateway service

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use("/user", expressProxy("http://localhost:3001"));
 app.use("/captain", expressProxy("http://localhost:3002"));
+app.use("/ride", expressProxy("http://localhost:3003"));
 
 app.listen(3000, (err) => {
   if (err) console.log("error when running the server", err);
